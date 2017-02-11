0efd5540a4aa4ee2ad51d12e5366e1dd887106c8
Marcin Moskal <mmos@memleak.pl>
2017-02-11 20:17:03 +0100

    Add phone glyphs + KALWI POC

--- atmega-meteo.c
+++ atmega-meteo.c
@@ -5,6 +5,13 @@
 FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 FILE lcd_str = FDEV_SETUP_STREAM(lcd_putchar, NULL, _FDEV_SETUP_WRITE);
 
+const uint8_t glyphs[][8] = {
+  { 0,    0,    0,    0x1f, 0x1f, 0x18, 0x18, 0x18 },
+  { 0,    0x03, 0x03, 0x1f, 0x1f, 0x03, 0x03, 0x03 },
+  { 0x1f, 0x1a, 0x1f, 0x1a, 0x1f, 0x1a, 0x1f, 0x1f },
+  { 0x1f, 0x0b, 0x1f, 0x0b, 0x1f, 0x0b, 0x1f, 0x1f }
+};
+
 static void ioinit(void) {
   lcd_init();
   uart_init();
@@ -16,6 +23,8 @@ int main(void) {
 
   stdout = stdin = &uart_str;
   stderr = &lcd_str;
+
+  lcd_setglyphs(glyphs, sizeof(glyphs) / sizeof(glyphs[0]));
 
   while(1) {
     char c = getchar();
