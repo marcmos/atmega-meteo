e926c00ec7b7e1c37eb089e9fa39034952a7e6e7
Marcin Moskal <mmos@memleak.pl>
2018-01-28 12:17:42 +0100

    Use 16-bit unsigned integers for pollution measurements

--- atmega-meteo-board/src/Pollution.hpp
+++ atmega-meteo-board/src/Pollution.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 class Pollution {
 public:
   Pollution():
@@ -8,13 +10,13 @@ public:
 
   bool initialized() const { return _initialized; }
 
-  unsigned char pm_2_5() const { return _pm_2_5; }
-  unsigned char pm_10() const { return _pm_10; }
+  uint16_t pm_2_5() const { return _pm_2_5; }
+  uint16_t pm_10() const { return _pm_10; }
 
-  unsigned char pm_2_5_percent() const { return _pm_2_5 * 100 / PM_2_5_LIMIT; }
-  unsigned char pm_10_percent() const { return _pm_10 * 100 / PM_10_LIMIT; }
+  uint16_t pm_2_5_percent() const { return _pm_2_5 * 100 / PM_2_5_LIMIT; }
+  uint16_t pm_10_percent() const { return _pm_10 * 100 / PM_10_LIMIT; }
 
-  void set(unsigned char pm_2_5, unsigned char pm_10) {
+  void set(uint16_t pm_2_5, uint16_t pm_10) {
     _pm_2_5 = pm_2_5;
     _pm_10 = pm_10;
     _initialized = true;
@@ -22,8 +24,8 @@ public:
 
 private:
   bool _initialized;
-  unsigned char _pm_2_5, _pm_10;
+  uint16_t _pm_2_5, _pm_10;
 
-  static const unsigned char PM_2_5_LIMIT = 25;
-  static const unsigned char PM_10_LIMIT = 50;
+  static const uint16_t PM_2_5_LIMIT = 25;
+  static const uint16_t PM_10_LIMIT = 50;
 };
